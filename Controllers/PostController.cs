9f72a55400dec854d6f4ff572f131b165354c662
Gilberto Hernández <joseg.hernandez@udem.edu>
2019-10-10 15:45:48 -0500

    Got a New CRUD

---
+++ Controllers/PostController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIStarter.Models;
+
+namespace WebAPIStarter.Controllers
+{
+    [ApiController]
+    [Route("api/{controller}")]
+    public class PostController : ControllerBase
+    {
+
+        private List<Post> AllPosts { get; set; }
+
+        [HttpGet]
+        public IEnumerable<Post> GetAll(){
+            return this.AllPosts;
+        }
+
+        [HttpGet("{id}")]
+        public Post GetSpecific(int id){
+            return this.AllPosts.Find( x => x.Id == id );
+        }
+
+        [HttpPost]    //Had to leave for a moment
+        public string CreatePost(Post post){
+            try{
+                this.AllPosts.Add(post);
+                return "AOK";
+            }catch(Exception e){
+                return $"Not AOK because of error {e.Message}";
+            }
+            
+        }
+
+        [HttpPut]
+        public string ChangePost(Post post){
+            try{
+                var myRef = this.AllPosts.Find( x => x.Id == post.Id );
+                myRef.Data = post.Data;
+                return "AOK";
+            }catch(Exception e){
+                return $"Not AOK because of error {e.Message}";
+            }
+        }
+
+        [HttpDelete]
+        public string DeletePost(int id){
+            try{
+                this.AllPosts.Remove(this.AllPosts.Find( x => x.Id == id ));
+                return "AOK no more";
+            }catch(Exception e){
+                return $"Not AOK because of error {e.Message}";
+            }
+        }
+        
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,8 @@
+namespace WebAPIStarter.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public object Data { get; set; }
+    }
+}
