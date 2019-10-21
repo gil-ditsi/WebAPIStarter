ecc699cee8fdfdf52c2b823fe4246b0600652337
Gilberto Hernández <joseg.hernandez@udem.edu>
2019-10-21 17:46:59 -0500

    Added migration with new FK

---
+++ Migrations/20191021212804_Relationships.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPIStarter.Migrations
+{
+    public partial class Relationships : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if(ActiveProvider == "Microsoft.EntityFrameworkCore.Sqlite"){
+                migrationBuilder.DropTable(
+                    name: "Addresses");
+                migrationBuilder.CreateTable(
+                    name: "Addresses",
+                    columns: table => new
+                    {
+                        Id = table.Column<long>(nullable: false)
+                            .Annotation("Sqlite:Autoincrement", true),
+                        Line1 = table.Column<string>(nullable: false, maxLength: 100),
+                        Line2 = table.Column<string>(nullable: true, maxLength: 100),
+                        City = table.Column<string>(nullable: true, maxLength: 50),
+                        StateProvice = table.Column<string>(nullable: true, maxLength: 50),
+                        Zipcode = table.Column<string>(nullable: true, maxLength: 10),
+                        AddressTypeId = table.Column<int>(nullable: false, defaultValue: 0)
+                    },
+                    constraints: table =>
+                    {
+                        table.PrimaryKey("PK_Addresses", x => x.Id);
+                    });
+            }else{
+                migrationBuilder.AlterColumn<string>(
+                    name: "Line1",
+                    table: "Addresses",
+                    maxLength: 100,
+                    nullable: false,
+                    oldClrType: typeof(string),
+                    oldType: "TEXT",
+                    oldNullable: true);
+            
+                migrationBuilder.AddColumn<int>(
+                    name: "AddressTypeId",
+                    table: "Addresses",
+                    nullable: false,
+                    defaultValue: 0);
+            }
+
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Addresses",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AddressTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AddressTypeName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AddressTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerAddresses",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(nullable: false),
+                    AddressId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerAddresses", x => new { x.CustomerId, x.AddressId });
+                    table.ForeignKey(
+                        name: "FK_CustomerAddresses_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CustomerAddresses_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_AddressTypeId",
+                table: "Addresses",
+                column: "AddressTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAddresses_AddressId",
+                table: "CustomerAddresses",
+                column: "AddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_AddressTypes_AddressTypeId",
+                table: "Addresses",
+                column: "AddressTypeId",
+                principalTable: "AddressTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_AddressTypes_AddressTypeId",
+                table: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "AddressTypes");
+
+            migrationBuilder.DropTable(
+                name: "CustomerAddresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_AddressTypeId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "AddressTypeId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Line1",
+                table: "Addresses",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+    }
+}
---
+++ Migrations/20191021223713_PostComment.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPIStarter.Migrations
+{
+    public partial class PostComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Posts",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PostId",
+                table: "Comments",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "Comments",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PostId",
+                table: "Comments",
+                column: "PostId");
+
+            if(ActiveProvider == "Microsoft.EntityFrameworkCore.Sqlite"){
+                migrationBuilder.DropTable(
+                    name: "Comments");
+                migrationBuilder.CreateTable(
+                    name: "Comments",
+                    columns: table => new
+                    {
+                        Id = table.Column<int>(nullable: false)
+                            .Annotation("Sqlite:Autoincrement", true),
+                        PostId = table.Column<string>(nullable: false, defaultValue: 0),
+                        Text = table.Column<string>(nullable: true)
+                    },
+                    constraints: table =>
+                    {
+                        table.PrimaryKey("PK_Comments", x => x.Id);
+                        table.ForeignKey(
+                            name: "FK_Comments_Posts_PostId",
+                            column: t => t.PostId,
+                            principalTable: "Posts",
+                            principalColumn: "Id",
+                            onDelete: ReferentialAction.Cascade);
+                    });
+            }else{
+                migrationBuilder.AddForeignKey(
+                    name: "FK_Comments_Posts_PostId",
+                    table: "Comments",
+                    column: "PostId",
+                    principalTable: "Posts",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+
+            }
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "PostId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "Comments");
+        }
+    }
+}
